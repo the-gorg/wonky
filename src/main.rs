c349c76c7292392282836fc0d6e5b9f345f06d8e
gorg <gorgmakesthings@gmail.com>
2021-04-24 17:01:32 +0200

    Formatter be formatting

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use anyhow::Result;
 use chrono::{Local, Timelike};
 use rand::Rng;
 
-use tinybit::events::{events, Event, EventModel, KeyCode, KeyEvent, KeyModifiers};
+use tinybit::events::{
+    events, Event, EventModel, KeyCode, KeyEvent, KeyModifiers,
+};
 use tinybit::render::{Renderer, StdoutTarget};
 use tinybit::widgets::Text;
 use tinybit::{term_size, Color, ScreenPos, ScreenSize, Viewport};
@@ -19,7 +21,8 @@ fn main() -> Result<()> {
     let (mut width, mut height) = term_size()?;
     let target = StdoutTarget::new()?;
     let mut renderer = Renderer::new(target);
-    let mut viewport = Viewport::new(ScreenPos::zero(), ScreenSize::new(width, height));
+    let mut viewport =
+        Viewport::new(ScreenPos::zero(), ScreenSize::new(width, height));
 
     let mut conf = settings::load()?;
 
@@ -43,7 +46,8 @@ fn main() -> Result<()> {
     let sleepy_time = 0..7;
 
     #[allow(unused_variables)]
-    let mut blocky_theme = MeterTheme::default(width as u8, "Progress:");
+    let mut blocky_theme =
+        MeterTheme::default((width / 2 - 2) as u8, "Progress:");
     let mut normal_theme = MeterTheme::halfblock((width / 2 - 2) as u8, "");
 
     let mut timer = std::time::Instant::now();
@@ -57,7 +61,8 @@ fn main() -> Result<()> {
 
                     let increment = if bottom { -1 } else { 1 };
 
-                    let vertical_pos = if bottom { height as i16 - 2 } else { 0 };
+                    let vertical_pos =
+                        if bottom { height as i16 - 2 } else { 0 };
                     let horizontal_pos = if right { width / 2 + 3 } else { 0 };
 
                     let mut i = 0;
@@ -67,11 +72,21 @@ fn main() -> Result<()> {
                             Widget::Meter(m) => {
                                 m.update()?;
                                 viewport.draw_widget(
-                                    &Text::new(m.title.clone(), fg_color(), None),
-                                    ScreenPos::new(horizontal_pos, (vertical_pos + (i)) as u16),
+                                    &Text::new(
+                                        m.title.clone(),
+                                        fg_color(),
+                                        None,
+                                    ),
+                                    ScreenPos::new(
+                                        horizontal_pos,
+                                        (vertical_pos + (i)) as u16,
+                                    ),
                                 );
                                 let test = Text::new(
-                                    format!("{}/{}{}", m.current_value, m.max_value, m.unit),
+                                    format!(
+                                        "{}/{}{}",
+                                        m.current_value, m.max_value, m.unit
+                                    ),
                                     fg_color(),
                                     None,
                                 );
@@ -79,7 +94,10 @@ fn main() -> Result<()> {
                                     &test,
                                     ScreenPos::new(
                                         // TODO: why 3?!?
-                                        horizontal_pos + (width / 2 - 3 - test.0.len() as u16),
+                                        horizontal_pos
+                                            + (width / 2
+                                                - 3
+                                                - test.0.len() as u16),
                                         (vertical_pos + i) as u16,
                                     ),
                                 );
@@ -106,6 +124,7 @@ fn main() -> Result<()> {
                                         (vertical_pos + 6 + (i)) as u16,
                                     ),
                                 );
+
                                 i += increment * 2;
                             }
                             Widget::Indicator(_) => {
@@ -124,15 +143,21 @@ fn main() -> Result<()> {
                         if sleepy_time.contains(&Local::now().hour()) {
                             let mut rng = rand::thread_rng();
                             if rng.gen_range(0..350) == 199 {
-                                bloatie.play_animation(BloatieAnimation::sleep_alt());
+                                bloatie.play_animation(
+                                    BloatieAnimation::sleep_alt(),
+                                );
                             } else {
-                                bloatie.play_animation(BloatieAnimation::sleep());
+                                bloatie
+                                    .play_animation(BloatieAnimation::sleep());
                             }
                         } else {
                             let mut rng = rand::thread_rng();
-                            if timer.elapsed().as_secs() > 10 && rng.gen_range(0..100) > 95 {
+                            if timer.elapsed().as_secs() > 10
+                                && rng.gen_range(0..100) > 95
+                            {
                                 timer = std::time::Instant::now();
-                                bloatie.play_animation(BloatieAnimation::idle());
+                                bloatie
+                                    .play_animation(BloatieAnimation::idle());
                             }
                         }
                     }
@@ -143,7 +168,9 @@ fn main() -> Result<()> {
             }
             Event::Key(KeyEvent { code, modifiers }) => match code {
                 KeyCode::Enter | KeyCode::Char('q') => return Ok(()),
-                KeyCode::Char('c') if modifiers == KeyModifiers::CONTROL => return Ok(()),
+                KeyCode::Char('c') if modifiers == KeyModifiers::CONTROL => {
+                    return Ok(())
+                }
                 KeyCode::Char('f') => bloatie.speak("It works!!"),
                 _ => {}
             },
